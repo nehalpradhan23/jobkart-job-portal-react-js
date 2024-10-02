dd5a34ed4dd99fb2162f261dd1a60b7342a12590
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-10-02 20:23:23 +0530

    add job function

---
+++ src/components/add-company-drawer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddCompanyDrawer = () => {
+  return <div>AddCompanyDrawer</div>;
+};
+
+export default AddCompanyDrawer;
--- src/pages/post-job.jsx
+++ src/pages/post-job.jsx
@@ -1,7 +1,185 @@
-import React from "react";
+import { getCompanies } from "@/api/apiCompanies";
+import { addNewJob } from "@/api/apiJobs";
+import AddCompanyDrawer from "@/components/add-company-drawer";
+import { Button } from "@/components/ui/button";
+
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import useFetch from "@/hooks/use-fetch";
+import { useUser } from "@clerk/clerk-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import MDEditor from "@uiw/react-md-editor";
+import { State } from "country-state-city";
+import { useEffect } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { Navigate, useNavigate } from "react-router-dom";
+import { BarLoader } from "react-spinners";
+import { z } from "zod";
+
+const schema = z.object({
+  title: z.string().min(1, { message: "Title is required" }),
+  description: z.string().min(1, { message: "Description is required" }),
+  location: z.string().min(1, { message: "Select a location" }),
+  company_id: z.string().min(1, { message: "Select or Add a new Company" }),
+  requirements: z.string().min(1, { message: "Requirements are required" }),
+});
 
 const PostJob = () => {
-  return <div>PostJob</div>;
+  const { user, isLoaded } = useUser();
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    defaultValues: { location: "", company_id: "", requirements: "" },
+    resolver: zodResolver(schema),
+  });
+
+  const {
+    loading: loadingCreateJob,
+    error: errorCreateJob,
+    data: dataCreateJob,
+    fn: fnCreateJob,
+  } = useFetch(addNewJob);
+
+  const onSubmit = (data) => {
+    fnCreateJob({
+      ...data,
+      recruiter_id: user.id,
+      isOpen: true,
+    });
+  };
+
+  // after successful job creation ----------------------------
+  useEffect(() => {
+    if (dataCreateJob?.length > 0) navigate("/jobs");
+  }, [loadingCreateJob]);
+
+  const {
+    loading: loadingCompanies,
+    data: companies,
+    fn: fnCompanies,
+  } = useFetch(getCompanies);
+
+  useEffect(() => {
+    if (isLoaded) {
+      fnCompanies();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoaded]);
+
+  if (!isLoaded || loadingCompanies) {
+    return <BarLoader className="mb-4" width={"100%"} color="white" />;
+  }
+
+  if (user?.unsafeMetadata?.role !== "recruiter") {
+    return <Navigate to="/jobs" />;
+  }
+
+  return (
+    <div className="wrapper mb-5">
+      <h1 className="font-bold text-3xl sm:text-5xl pb-8 mt-5">Post a Job</h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-4 py-4 pb-0"
+      >
+        <Input placeholder="Job Title" {...register("title")} />
+        {errors.title && <p className="text-red-500">{errors.title.message}</p>}
+
+        <Textarea placeholder="Job Description" {...register("description")} />
+        {errors.description && (
+          <p className="text-red-500">{errors.description.message}</p>
+        )}
+
+        <div className="flex gap-4 items-center max-md:flex-col">
+          <Controller
+            name="location"
+            control={control}
+            render={({ field }) => (
+              <Select value={field.value} onValueChange={field.onChange}>
+                <SelectTrigger>
+                  <SelectValue placeholder="Job Location" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup className="h-[150px]">
+                    {State.getStatesOfCountry("IN").map(({ name }) => (
+                      <SelectItem key={name} value={name}>
+                        {name}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            )}
+          />
+          <Controller
+            name="company_id"
+            control={control}
+            render={({ field }) => (
+              <Select value={field.value} onValueChange={field.onChange}>
+                <SelectTrigger>
+                  <SelectValue placeholder="Company">
+                    {field.value
+                      ? companies?.find((com) => com.id === Number(field.value))
+                          ?.name
+                      : "Company"}
+                  </SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup className="h-[150px]">
+                    {companies?.map(({ name, id }) => (
+                      <SelectItem key={name} value={id}>
+                        {name}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            )}
+          />
+          <AddCompanyDrawer fetchCompanies={fnCompanies} />
+        </div>
+        {errors.location && (
+          <p className="text-red-500">{errors.location.message}</p>
+        )}
+        {errors.company_id && (
+          <p className="text-red-500">{errors.company_id.message}</p>
+        )}
+
+        <Controller
+          name="requirements"
+          control={control}
+          render={({ field }) => (
+            <MDEditor value={field.value} onChange={field.onChange} />
+          )}
+        />
+        {errors.requirements && (
+          <p className="text-red-500">{errors.requirements.message}</p>
+        )}
+        {errors.errorCreateJob && (
+          <p className="text-red-500">{errors?.errorCreateJob?.message}</p>
+        )}
+        {errorCreateJob?.message && (
+          <p className="text-red-500">{errorCreateJob?.message}</p>
+        )}
+        {loadingCreateJob && <BarLoader width={"100%"} color="white" />}
+        <Button type="submit" className="my-2 text-lg md:text-xl">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default PostJob;
